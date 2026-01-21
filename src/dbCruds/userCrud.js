4a087ae679a91a54d09c16d1fb5fb3ac4e888d93
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2026-01-21 17:02:26 +0100

    Create userCrud.js

---
+++ src/dbCruds/userCrud.js
@@ -0,0 +1,38 @@
+const { BankAccount, Wallet } = require('../models/userModel');
+
+// get bank account by userId, if not found create one
+const getBankAccountByUser = async (user) => {
+  let bankAccount = await BankAccount.findOne({ userId: user._id });
+  if (!bankAccount) {
+    const fullName = `${user.lastName} ${user.firstName}`;
+    const bankAccount = new BankAccount({
+      userId: user._id,
+      accountName: fullName,
+    });
+    await bankAccount.save();
+  }
+  return bankAccount.toJSON();
+};
+
+// get bank account by account number
+const getBankAccountByAccountNumber = async (accountNumber) => {
+  return await BankAccount.findOne({ accountNumber });
+};
+
+const getUserWalletByUser = async (user) => {
+  const wallet = await Wallet.findOne({ userId: user._id });
+  if (!wallet) {
+    const newWallet = new Wallet({
+      userId: user._id,
+      balance: 0,
+    });
+    return await newWallet.save();
+  }
+  return wallet.toJSON();
+};
+
+module.exports = {
+  getBankAccountByUser,
+  getUserWalletByUser,
+  getBankAccountByAccountNumber,
+};
