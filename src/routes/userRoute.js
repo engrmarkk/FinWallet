336883b4734b43ba3942955bd055f6b0417c395d
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2026-01-21 17:03:14 +0100

    Update userRoute.js

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { getUserDetails } = require('../controllers/userController');
+const {
+  getUserDetails,
+  getBanksController,
+  resolveAccountNumberController,
+  getMyBankDetailsController,
+  getMyWalletDetailsController,
+} = require('../controllers/userController');
 const { authenticate } = require('../middlewares/authHandler');
 
 router.get('/me', authenticate, getUserDetails);
-
+router.get('/banks', authenticate, getBanksController);
+router.get('/resolve-account', authenticate, resolveAccountNumberController);
+router.get('/my-bank-details', authenticate, getMyBankDetailsController);
+router.get('/my-wallet-details', authenticate, getMyWalletDetailsController);
 module.exports = router;
