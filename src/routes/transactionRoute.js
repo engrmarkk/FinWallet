836d801d29852b5d3b192e91f50b76e229e1be81
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2026-01-22 19:56:20 +0100

    Update transactionRoute.js

--- src/routes/transactionRoute.js
+++ src/routes/transactionRoute.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getTransactionCategoriesController } = require('../controllers/transactionController');
+const {
+  getTransactionCategoriesController,
+  transferController,
+} = require('../controllers/transactionController');
 const { authenticate } = require('../middlewares/authHandler');
 
 router.get('/transaction-categories', authenticate, getTransactionCategoriesController);
+router.post('/transfer', authenticate, transferController);
 
 module.exports = router;
