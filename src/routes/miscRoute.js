0115beac89c1bc7a005cdfb19ccd6646d9a3b3ca
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2026-01-24 12:55:00 +0100

    Update miscRoute.js

--- src/routes/miscRoute.js
+++ src/routes/miscRoute.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { createTransactionCategoryController } = require('../controllers/transactionController');
-const { topUpController } = require('../controllers/transactionController');
+const {
+  createTransactionCategoryController,
+} = require('../controllers/transactions/transactionController');
+const { topUpController } = require('../controllers/transactions/transactionController');
 
 router.post('/transaction-category', createTransactionCategoryController);
 router.post('/top-up', topUpController);
