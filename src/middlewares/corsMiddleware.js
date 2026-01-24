6900355b496641a33ccf6973ee4d9a1779c879f2
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2026-01-24 12:53:58 +0100

    Create corsMiddleware.js

---
+++ src/middlewares/corsMiddleware.js
@@ -0,0 +1,16 @@
+const cors = require('cors');
+
+const allowedOrigins = ['http://localhost:5173'];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+    callback(new Error('Not allowed by CORS'));
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+};
+
+module.exports = cors(corsOptions);
