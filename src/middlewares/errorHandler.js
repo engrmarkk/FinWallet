a34d8bf7ac135a332484a6c374630c6863a28764
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2026-01-24 12:54:56 +0100

    Update errorHandler.js

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,15 +1,18 @@
 const { apiResponse } = require('../utils/apiResponse');
 const HttpStatusCodes = require('../utils/statusCodes');
 const StatusResponse = require('../utils/statusResponse');
+const Logger = require('../utils/logger');
+
+const logger = new Logger();
 
 // middleware/errorHandler.js
 function errorHandler(err, req, res, next) {
-  console.error(err);
+  logger.error(err);
 
   // Mongoose validation error
   if (err.name === 'ValidationError') {
     const messages = Object.values(err.errors).map((e) => e.message);
-    console.log(`Validation errors: ${messages}`);
+    logger.info(`Validation errors: ${messages}`);
     return apiResponse(res, 'Network Error', HttpStatusCodes.BAD_REQUEST, StatusResponse.FAILED);
   }
 
