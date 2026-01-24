1c9ffb9e9100fc372eb53101bf1c35570670343e
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2026-01-24 21:06:41 +0100

    Update billTransactionRoute.js

--- src/routes/transactions/billTransactionRoute.js
+++ src/routes/transactions/billTransactionRoute.js
@@ -4,10 +4,12 @@ const {
   getServicesController,
   getVariationsController,
   verifyMeterAndSmartcardNumberController,
+  purchaseBillController
 } = require('../../controllers/transactions/billTransactionController');
 const { authenticate } = require('../../middlewares/authHandler');
 
 router.get('/services/:service', authenticate, getServicesController);
 router.get('/variations/:serviceID', authenticate, getVariationsController);
 router.post('/verify-meter-smartcard', authenticate, verifyMeterAndSmartcardNumberController);
+router.post('/purchase-bill', authenticate, purchaseBillController);
 module.exports = router;
