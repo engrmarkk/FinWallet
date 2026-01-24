9cbf63aaffe34d2cb6f0e9a735dcad534e98f878
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2026-01-24 12:54:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,18 +4,25 @@ const errorHandler = require('./src/middlewares/errorHandler');
 const pingRoute = require('./src/routes/pingRoute');
 const authRoute = require('./src/routes/authRoute');
 const userRoute = require('./src/routes/userRoute');
-const transactionRoute = require('./src/routes/transactionRoute');
+const transactionRoute = require('./src/routes/transactions/transactionRoute');
+const billTransactionRoute = require('./src/routes/transactions/billTransactionRoute');
 const miscRoute = require('./src/routes/miscRoute');
-
+const corsMiddleware = require('./src/middlewares/corsMiddleware');
 const express = require('express');
+const Logger = require('./src/utils/logger');
+
+const logger = new Logger();
+
 const app = express();
+
+app.use(corsMiddleware);
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DATABASE_URI);
-    console.log('MongoDB connected successfully');
+    logger.info('MongoDB connected successfully');
   } catch (error) {
-    console.error('MongoDB connection failed:', error.message);
+    logger.error('MongoDB connection failed:', error.message);
     process.exit(1);
   }
 };
@@ -30,12 +37,13 @@ app.use(`${apiVersion}/ping`, pingRoute);
 app.use(`${apiVersion}/auth`, authRoute);
 app.use(`${apiVersion}/user`, userRoute);
 app.use(`${apiVersion}/transactions`, transactionRoute);
+app.use(`${apiVersion}/bill-transactions`, billTransactionRoute);
 app.use(`${apiVersion}/misc`, miscRoute);
 
 app.use(errorHandler);
 // listien to connection 'open' event, if its connected, run the server
 mongoose.connection.on('open', () => {
   app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    logger.info(`Server is running on port ${PORT}`);
   });
 });
