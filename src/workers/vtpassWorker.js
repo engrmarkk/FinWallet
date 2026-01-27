efaa2a70386c7aaa0e24becc6e7f254619b9f2ac
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2026-01-27 14:25:28 +0100

    Create vtpassWorker.js

---
+++ src/workers/vtpassWorker.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const { Worker } = require('bullmq');
+const { connection } = require('../config/redis.js');
+const { requeryVtpassTransaction } = require('../dbCruds/backgroundCrud.js');
+const { saveVtpassResponse } = require('../dbCruds/backgroundCrud');
+const Logger = require('../utils/logger');
+
+const logger = new Logger();
+
+(async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URI);
+    logger.info('MongoDB connected successfully@vtpassWorker');
+
+    const vtpassWorker = new Worker(
+      'vtpass-requery',
+      async (job) => {
+        logger.info(`job data: ${JSON.stringify(job)}`);
+        if (job.name === 'requery') {
+          return await requeryVtpassTransaction(job.data);
+        } else if (job.name === 'saveResponse') {
+          return await saveVtpassResponse(job.data.request_id, job.data.content);
+        }
+      },
+      { connection }
+    );
+
+    vtpassWorker.on('completed', (job) => {
+      console.log(`Job ${job.id} completed`);
+    });
+
+    vtpassWorker.on('failed', (job, err) => {
+      console.log(`Job ${job.id} failed: ${err.message}`);
+    });
+
+  } catch (error) {
+    logger.error('MongoDB connection failed@vtpassWorker: ' + error.message);
+    process.exit(1);
+  }
+})();
+
+
+// module.exports = { vtpassWorker };
