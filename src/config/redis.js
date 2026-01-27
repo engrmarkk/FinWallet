ec067f48420b3080dd384af8d12650f29fea7241
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2026-01-27 14:24:36 +0100

    Create redis.js

---
+++ src/config/redis.js
@@ -0,0 +1,10 @@
+const Redis = require('ioredis');
+
+const connection = new Redis({
+  host: process.env.REDIS_HOST || '127.0.0.1',
+  port: process.env.REDIS_PORT || 6379,
+  maxRetriesPerRequest: null,
+  enableReadyCheck: false
+});
+
+module.exports = { connection };
