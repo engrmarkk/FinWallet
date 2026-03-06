d7f244598998c7102d2572766a6b5f85733021a4
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2026-03-06 15:20:35 +0100

    Update authHandler.js

--- src/middlewares/authHandler.js
+++ src/middlewares/authHandler.js
@@ -2,11 +2,17 @@ const jwt = require('jsonwebtoken');
 const { User } = require('../models/userModel');
 const { apiResponse } = require('../utils/apiResponse');
 const HttpStatusCodes = require('../utils/statusCodes');
+const Logger = require('../utils/logger');
 const StatusResponse = require('../utils/statusResponse');
+const { connection } = require('../config/redis.js');
+
+const logger = new Logger();
 
 const authenticate = async (req, res, next) => {
   try {
+    logger.info('Authenticating user...');
     let token = req.header('Authorization') || '';
+    const userAgent = req.headers['user-agent'] || 'unknown';
     if (token.startsWith('Bearer ')) token = token.slice(7);
 
     if (!token) {
@@ -19,6 +25,35 @@ const authenticate = async (req, res, next) => {
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const tokenUserAgent = decoded.userAgent || 'unknown';
+    const jti = decoded.jti;
+    logger.info(`Authenticating user with ID: ${decoded.id} and User-Agent: ${tokenUserAgent}`);
+
+    // check if the token's jti is in the blacklist (for logout)
+    const isBlacklisted = await connection.get(`blacklist_${jti}`);
+    if (isBlacklisted) {
+      logger.info(
+        `Token has been revoked. Please log in again. JTI: ${jti}, isBlacklisted: ${isBlacklisted}`
+      );
+      return apiResponse(
+        res,
+        'Token has been revoked. Please log in again.',
+        HttpStatusCodes.UNAUTHORIZED,
+        StatusResponse.FAILED
+      );
+    }
+
+    if (tokenUserAgent !== userAgent) {
+      logger.info(
+        `Token is not valid for this device. Expected User-Agent: ${tokenUserAgent}, Received User-Agent: ${userAgent}`
+      );
+      return apiResponse(
+        res,
+        'Token is not valid for this device.',
+        HttpStatusCodes.UNAUTHORIZED,
+        StatusResponse.FAILED
+      );
+    }
     const user = await User.findById(decoded.id).select('-password');
 
     if (!user) {
@@ -50,8 +85,10 @@ const authenticate = async (req, res, next) => {
 
     req.user = user;
     req.token = token;
+    req.user.jti = jti;
     next();
   } catch (error) {
+    // logger.error(`Authentication error: ${error}`);
     if (error.name === 'JsonWebTokenError') {
       return apiResponse(
         res,
