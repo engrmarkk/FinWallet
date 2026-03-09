d7d2a1d9f89c30f4380ea46a7b5efcf4966211e1
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2026-03-09 21:51:56 +0100

    Update apiResponse.js

--- src/utils/apiResponse.js
+++ src/utils/apiResponse.js
@@ -1,7 +1,17 @@
-exports.apiResponse = (res, message, statusCode, statusMessage = 'success', data = null) => {
+exports.apiResponse = (
+  res,
+  message,
+  statusCode,
+  statusMessage = 'success',
+  data = null,
+  pagination = null
+) => {
   const response = { status: statusMessage, msg: message };
   if (data) {
     response.data = data;
+  }
+  if (pagination) {
+    response.pagination = pagination;
   }
   res.status(statusCode).json(response);
 };
