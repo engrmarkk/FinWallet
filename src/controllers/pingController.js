606439f9f21e49cd388afbb736ae881fa7751bd6
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2026-03-16 22:24:24 +0100

    Update pingController.js

--- src/controllers/pingController.js
+++ src/controllers/pingController.js
@@ -1,6 +1,19 @@
-const express = require('express');
 const { apiResponse } = require('../utils/apiResponse');
+const { emailQueue } = require('../queues/email_queue');
 
 exports.ping = async (req, res) => {
+  // send otp
+  await emailQueue.add('send_email', {
+    email: 'mjay4k@gmail.com',
+    subject: 'Test Email from FinWallet',
+    htmlContent: null,
+    templatePath: 'otpEmail.html',
+    templateContext: {
+      firstName: 'John',
+      lastName: 'Doe',
+      otp: '123456',
+      expiryMinutes: 10,
+    },
+  });
   apiResponse(res, 'pong', 200);
 };
